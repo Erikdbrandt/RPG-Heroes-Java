6ef5443ad3b58a360ebcd7d251711c2e77778f15
Erik Brandt <erikdbrandt@gmail.com>
2023-02-07 14:56:47 +0100

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Mage mage = new Mage();
+
+        mage.setName("Gandalf");
+
+        mage.display();
+
+
+
+    }
+}
